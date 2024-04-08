b9e514ae5739fb2f4a23f49edbacad9418e41e17
Yehoshuaro <231132@astanait.edu.kz>
2024-04-08 22:57:50 +0500

    First Task Accepted

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
+    public static int Sum_Function(int n) {
+        if (n == 0) {
+            return 0;
+        } else {
+            return n * n + Sum_Function(n - 1);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter n: ");
+        int n = scanner.nextInt();
+        int sum = Sum_Function(n);
+
+        String output = "";
+        for (int i = 1; i <= n; i++) {
+            output += i + "^2";
+            if (i < n) {
+                output += " + ";
+            }
+        }
+        output += " = " + sum;
+
+
+        System.out.println(output);
     }
 }
+
