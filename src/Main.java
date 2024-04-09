7a39fc48e16840062dd91c8cd4cf699f35d10544
Yehoshuaro <231132@astanait.edu.kz>
2024-04-09 15:06:06 +0500

    Fifth Task [Accepted]

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-// I used recursion now
-    public static int Pomodoro(int b, int n) {
-        if (n < 0) {
-            return 0;
-        } else {
-            return (int)(Math.pow(b, n) + Pomodoro(b, n - 1));
-        }
-    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter b: ");
-        int b = scanner.nextInt();
-
-        System.out.print("Enter n: ");
         int n = scanner.nextInt();
 
-        int sum = Pomodoro(b, n);
+        Reverse(scanner, n);
+    }
 
-        System.out.print(b + "^0 + " + b + "^1 + ");
-        for (int x = 2; x < n; x++) {
-            System.out.print(b + "^" + x + " + ");
+    public static void Reverse(Scanner scanner, int n) {
+        if (n == 0) {
+            return;
         }
-        System.out.println(b + "^" + n + " = " + sum);
+
+        int current = scanner.nextInt();
+        Reverse(scanner, n - 1);
+        System.out.print(current + " ");
     }
 }
 
